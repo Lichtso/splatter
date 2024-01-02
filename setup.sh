c165c53bcbf2f57a54dd6f0b90a29490fa0a81a3
Victor A. <52110451+cs50victor@users.noreply.github.com>
2024-01-02 01:33:46 -0500

    feat: add setup shell rile

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!usr/bin/env sh
+
+# To run : curl https://url -sSf | sh
+
+
+sudo apt update
+
+# Install git & unzip
+
+sudo apt install git unzip
+
+# Install Cargo
+curl https://sh.rustup.rs -sSf | sh
+
+# download repo
+mkdir dev
+
+cd dev
+
+# Download and Unzip Models
+
+cd splatter
+
+wget https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/datasets/pretrained/models.zip
+
+unzip models.zip -d models
+
+# Run example
+
+cargo run --example showcase -- models/garden/point_cloud/iteration_7000/point_cloud.ply
